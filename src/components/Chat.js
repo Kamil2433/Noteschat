4893c082b8c5ce9ea30cf802bf3a76d695123425
Kamil2433 <136186707+Kamil2433@users.noreply.github.com>
2024-03-01 07:15:49 +0530

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { useChat } from "../context/Chatprovider";
-import { ListGroup, ListGroupItem, Button } from "react-bootstrap";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import Modal from 'react-bootstrap/Modal';  
+import Newchatmodal from "./Newchatmodal";
+import { useState } from "react";
 
 export default function Chat() {
   const { chat, setselectedchat, selectedchat, setselectedchatid,selectedgroup, setseectedgroup,setselectedname } = useChat();
@@ -9,7 +18,32 @@ export default function Chat() {
   //   setselectedchatid(data);
   // }
 
+  const [show,setmodal]=useState(false);
+
+
+  const handlemodal=()=>{
+
+     setmodal(true);
+
+
+  }
+
+  const closethemodal=()=>{
+
+setmodal(false);
+
+  }
+
   return (
+    <>
+
+<Modal  show={show} onHide={closethemodal}>  
+
+<Newchatmodal onHide={closethemodal}/>
+
+
+</Modal>
+
     <ListGroup>
       {chat.map((data, idx) => (
         <ListGroup.Item
@@ -24,7 +58,15 @@ export default function Chat() {
         >
           <div>{data.name}</div>
         </ListGroup.Item>
+         
       ))}
+
+
+
     </ListGroup>
+          <Fab color="primary" aria-label="add">
+            <AddIcon onClick={handlemodal} />
+          </Fab>
+         </>
   );
 }
